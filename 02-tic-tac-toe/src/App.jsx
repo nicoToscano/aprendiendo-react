f1f4f9bb02517c61951cf1c95d89fb14a6f63876
nicoToscano <nicolastoscano1@hotmail.com>
2024-05-01 17:44:59 -0500

    Reset and draw game

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -65,6 +65,19 @@ function App() {
     return null; //Si no hay ganador
   };
 
+  //Función para reiniciar el juego
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const checkEndGame = (newBoard) => {
+    //Revise si todos los cuadrados tienen un valor
+    //Si todos los cuadrados tienen un valor, entonces hay un empate
+    return newBoard.every((square) => square !== null);
+  };
+
   const updateBoard = (index) => {
     // Si el cuadrado ya tiene un valor, no se puede actualizar o si ya hay un ganador
     if (board[index] || winner) return;
@@ -84,18 +97,23 @@ function App() {
     if (newWinner) {
       //Mostrar un mensaje con el ganador
       // alert(`Ganó ${newWinner}`);
+
       //Actualizar el estado del ganador
-      // setWinner(newWinner);
-      setWinner((prevWinner) => {
-        console.log(`Ganador ${newWinner}, Ganador anterior ${prevWinner}`);
-        return newWinner;
-      });
+      setWinner(newWinner);
+
+      // setWinner((prevWinner) => {
+      //   console.log(`Ganador ${newWinner}, Ganador anterior ${prevWinner}`);
+      //   return newWinner;
+      // });
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false); //Empate
     }
   };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reiniciar juego</button>
       <section className="game">
         {board.map((_, index) => {
           return (
@@ -110,6 +128,22 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Empate" : "Gano: "}</h2>
+
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
